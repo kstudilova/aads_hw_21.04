0756268f18938d1a2c27f9642bb68f378f162e5c
kstudilova <kstudilova@gmail.com>
2026-05-04 04:07:02 +0300

    add demonstration programm

--- main.cpp
+++ main.cpp
@@ -5,17 +5,53 @@
 template< class T >
 struct BiTree
 {
-  T value;
+  T val;
   BiTree< T >* lt;
   BiTree< T >* rt;
   BiTree< T >* parent;
+  BiTree(T val);
 };
+
+template< class T >
+BiTree< T >::BiTree(T val):
+  val(val),
+  lt(nullptr),
+  rt(nullptr),
+  parent(nullptr)
+{}
 
 enum Dir 
 {
   fall_left,
   parent_dir
 };
+
+template< class T >
+BiTree<T>* makeNode(T val)
+{
+  return new BiTree< T >{val};
+}
+
+template< class T >
+void connect(BiTree< T >* parent, BiTree< T >* left, BiTree< T >* right)
+{
+  parent->lt = left;
+  parent->rt = right;
+  if (left){
+    left->parent = parent;
+  }
+  if (right){
+    right->parent = parent;
+  }
+}
+
+template< class T >
+void clear(BiTree< T >* root) {
+  if (!root) return;
+  clear(root->lt);
+  clear(root->rt);
+  delete root;
+}
 
 template< class T >
 std::pair< size_t, BiTree< T >* > fallLeft(BiTree< T >* node)
@@ -38,7 +74,7 @@ std::pair< size_t, BiTree< T >* > goParent( BiTree< T >* node)
   }
   size_t len = 0;
   BiTree< T >* p = node->parent;
-  while( node && p->rt != node)
+  while( p && p->rt != node)
   {
     node = p;
     p = node->parent;
@@ -60,6 +96,10 @@ std::tuple< Dir, size_t, BiTree< T >* > nextStruct( BiTree< T >* node)
     return { fall_left, res.first, res.second };
   }
   std::pair< size_t, BiTree< T >* > res = goParent(node);
+  if (!res.second)
+  {
+    return { parent_dir, res.first, nullptr };
+  }
   return { parent_dir, res.first, res.second };
 }
 
@@ -69,8 +109,8 @@ BiTree< T >* maximum(BiTree< T >* node)
   while (node && node->rt)
   {
     node = node->rt;
-    return node;
   }
+  return node;
 }
 
 template< class T >
@@ -96,6 +136,10 @@ BiTree< T >* prev(BiTree< T >* node)
 template< class T >
 std::tuple< BiTree< T >*, BiTree< T >*, bool > isEqualStructStart(BiTree< T >* lhs, BiTree< T >* rhs)
 {
+  if (!lhs || !rhs)
+  {
+  return {nullptr, nullptr, false};
+  }
   std::tuple< Dir, size_t, BiTree< T >* > ln = nextStruct(lhs);
   std::tuple< Dir, size_t, BiTree< T >* > rn = nextStruct(rhs);
   while (
@@ -107,13 +151,14 @@ std::tuple< BiTree< T >*, BiTree< T >*, bool > isEqualStructStart(BiTree< T >* l
     ln = nextStruct(std::get< 2 >(ln));
     rn = nextStruct(std::get< 2 >(rn));
   }
-  bool same_dir = std::get< 0 >(ln) == std::get< 0 >(rn);
+  bool same_dir = std::get< 0 >(ln) == std::get< 0 >(rn) && std::get<2>(rn) == nullptr;
   return { std::get< 2 >(ln), std::get< 2 >(rn), same_dir };
 }
 
 template< class T >
 std::pair< BiTree< T >*, BiTree< T >* > inclusionStart(BiTree< T >* start, BiTree< T >* pattern)
 {
+  if (!start) return {nullptr, nullptr};
   BiTree<  T>* pattern_begin = fallLeft(pattern).second;
   BiTree< T >* curr = start;
   while (curr)
@@ -169,6 +214,11 @@ InclusionIt< T > next(InclusionIt< T > it, BiTree< T >* pattern)
 
   BiTree< T >* next_start = std::get<2>(nextStruct(it.incl.first));
 
+  if (!next_start)
+  {
+    return { {nullptr, nullptr} };
+  }
+
   return { inclusionStart(next_start, pattern) };
 }
 
@@ -179,12 +229,67 @@ bool hasNext(InclusionIt<T> it, BiTree<T>* pattern)
 
   BiTree<T>* next_start = std::get<2>(nextStruct(it.incl.first));
 
-  auto res = inclusionStart(next_start, pattern);
+  if (!next_start) return false;
+  std::pair< BiTree< T >*, BiTree< T >* > res = inclusionStart(next_start, pattern);
 
   return res.first != nullptr;
 }
 
+template < class T >
+void printTree(BiTree< T >* n, int d = 0)
+{
+  if (!n) return;
+  std::cout << std::string(d * 2, ' ') << n->val << "\n";
+  if (n->lt)
+  {
+    std::cout << std::string(d * 2, ' ') << "L:";
+    printTree(n->lt, d + 1);
+  }
+  if (n->rt)
+  {
+    std::cout << std::string(d * 2, ' ') << "R:";
+    printTree(n->rt, d + 1);
+  }
+}
+
 int main() 
 {
-  
+  BiTree< int >* n1 = makeNode(1);
+  BiTree< int >* n4 = makeNode(4);
+  BiTree< int >* n3 = makeNode(3);
+  BiTree< int >* n6 = makeNode(6);
+  BiTree< int >* n2 = makeNode(2);
+  BiTree< int >* n5 = makeNode(5);
+
+  connect< int >(n1, (BiTree<int>*)nullptr, n4);
+  connect< int >(n4, n3, n6);
+  connect< int >(n3, n2, (BiTree<int>*)nullptr);
+  connect< int >(n6, n5, (BiTree<int>*)nullptr);
+
+  BiTree< int >* p2 = makeNode(2);
+
+
+  std::cout << "LHS tree:\n";
+  printTree(n1);
+
+  std::cout << "\nPattern:\n";
+  printTree(p2);
+
+  std::cout << "\nMatches:\n";
+
+  InclusionIt< int > it = begin(n1, p2);
+  if (!it.incl.first)
+  {
+  std::cout << "No matches\n";
+  }
+  while (it.incl.first)
+  {
+    std::cout << "Match from " << it.incl.first->val << " to " << it.incl.second->val << "\n";
+    it = next(it, p2);
+  }
+
+  clear(n1);
+  clear(p2);
+
+  return 0;
 }
